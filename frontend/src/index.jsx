712620b96d090dc23e2a8e1af87c83cf6599cdc5
Tushar Kumar Saini <tusharsaini9521@gmail.com>
2025-12-19 19:25:15 +0530

    add gitignore

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.css"; // <-- important
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
