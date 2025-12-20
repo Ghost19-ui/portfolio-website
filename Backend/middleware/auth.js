a89dba73ba87828c08eea95d5eb54caabb5c6ee2
Tushar Kumar Saini <tusharsaini9521@gmail.com>
2025-12-20 19:31:56 +0530

    Fix merge conflicts in auth middleware

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,10 +1,16 @@
-<<<<<<< HEAD
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.protect = async (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(' ');
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
 
     if (!token) {
       const err = new Error('Not authorized, no token');
@@ -36,42 +42,3 @@ exports.authorize = (...roles) => (req, res, next) => {
   }
   next();
 };
-=======
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-
-exports.protect = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(' ');
-
-    if (!token) {
-      const err = new Error('Not authorized, no token');
-      err.status = 401;
-      return next(err);
-    }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select('-password');
-
-    if (!req.user) {
-      const err = new Error('User not found');
-      err.status = 401;
-      return next(err);
-    }
-
-    next();
-  } catch (error) {
-    error.status = 401;
-    next(error);
-  }
-};
-
-exports.authorize = (...roles) => (req, res, next) => {
-  if (!req.user || !roles.includes(req.user.role)) {
-    const err = new Error('Not authorized for this action');
-    err.status = 403;
-    return next(err);
-  }
-  next();
-};
->>>>>>> dc566378 (`Initial commit of frontend package with dependencies and styles`)
